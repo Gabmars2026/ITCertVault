a38926932913c0a9ac6cf5ec48bb56222556c482
Gabmars2026 <gmajorenos@outlook.com>
2026-08-21 15:08:02 -0500

    Add pre-start local content gate fix

---
+++ CertForge/itcv-content-gate-fix.js
@@ -0,0 +1,6 @@
+(function(){
+  'use strict';
+  window.CF_BLOB=window.CF_BLOB||{};
+  window.CF_BLOB.manifest={};
+  document.documentElement.setAttribute('data-itcv-local-content','ready');
+})();
